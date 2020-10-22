8cb5e04e29f91c8ec069bd46b6a53a7e4d5700f2
Jorge Abdo <jorgeabdo@Jorges-MacBook-Pro.local>
2020-10-21 20:07:45 -0500

    login fix

--- admin/src/routes/middleware/Authmiddleware.js
+++ admin/src/routes/middleware/Authmiddleware.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Route, Redirect, withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import { compose } from "redux";
 
-const Authmiddleware = ({ component: Component, layout: Layout, profile }) => (
+const Authmiddleware = ({ component: Component, layout: Layout }) => (
   <Route
     render={(props) => {
       // here you can apply condition
@@ -16,10 +14,4 @@ const Authmiddleware = ({ component: Component, layout: Layout, profile }) => (
   />
 );
 
-const mapStateToProps = (state) => {
-  return {
-    profile: state.firebase.profile,
-  };
-};
-
-export default compose(connect(mapStateToProps), withRouter)(Authmiddleware);
+export default withRouter(Authmiddleware);
