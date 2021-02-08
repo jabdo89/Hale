319e91e2d6bdffef55e1aec0a575f15babf70c9a
JorgeGiovannetti <giovannetti2000@gmail.com>
2021-02-08 13:07:27 -0600

    Orders excel export

--- admin/src/components/Common/ExcelExport.js
+++ admin/src/components/Common/ExcelExport.js
@@ -2,22 +2,21 @@ import React from "react";
 import { Button } from "reactstrap";
 import XLSX from "xlsx";
 
-const ExcelExport = ({ data = [] }) => {
-  const fileExport = () => {
-    console.log("Exporting data", data);
+const defaultExcelExport = (data) => {
+  let worksheet = XLSX.utils.json_to_sheet(data);
+  let workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet);
+  XLSX.writeFile(workbook, "data.xlsx");
+};
 
-    let worksheet = XLSX.utils.json_to_sheet(data);
-    let workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet);
-    XLSX.writeFile(workbook, "datos.xlsx");
-  };
+const ExcelExport = ({ data = [], fileExport = defaultExcelExport}) => {
 
   return (
     <Button
       type="button"
       color="success"
       className="btn-rounded waves-effect waves-light mb-2 mr-2"
-      onClick={fileExport}
+      onClick={() => fileExport(data)}
     >
       Exportar a Excel
     </Button>
---
+++ admin/src/pages/Bern/Ordenes/OrdersExcelFormatter.js
@@ -0,0 +1,59 @@
+import XLSX from "xlsx";
+import moment from "moment";
+
+const OrdersExcelFormatter = (data) => {
+  let wb = XLSX.utils.book_new();
+  //   let ws = XLSX.utils.json_to_sheet(data);
+  let ws = XLSX.utils.aoa_to_sheet([[""]]);
+
+  ws["!merges"] = [
+    { s: { r: 1, c: 1 }, e: { r: 1, c: 8 } },
+    { s: { r: 1, c: 9 }, e: { r: 1, c: 18 } },
+  ];
+
+  XLSX.utils.sheet_add_aoa(ws, [["Detalles de pedido"]], { origin: "B2" });
+  XLSX.utils.sheet_add_aoa(ws, [["Detalles de envío"]], { origin: "J2" });
+
+  let cell_address = { c: 1, r: 2 };
+  let cell_ref = XLSX.utils.encode_cell(cell_address);
+
+  let transformedData = data.map((obj) => ({
+    ["#ORDEN"]: "",
+    ["Order ID"]: obj.id ?? "",
+    ["Fecha"]: moment(new Date(obj.date.seconds * 1000)).format("MMM Do YYYY") ?? "",
+    ["Cliente"]: `${obj.firstName ?? ''} ${obj.lastname ?? ''}`,
+    ["Producto"]:
+      `${obj.items[0].name}${obj.items[1] ? `, ${obj.items[1].name}` : ""}` ??
+      "",
+    ["Piezas"]:
+      `${obj.items[0].quantity}${
+        obj.items[1] ? `, ${obj.items[1].quantity}` : ""
+      }` ?? "",
+    ["Precio individual"]:
+      `${obj.items[0].price}${obj.items[1] ? `, ${obj.items[1].price}` : ""}` ??
+      "",
+    ["Direccion (1)"]: obj.address.street ?? "",
+    ["Direccion (2)"]: obj.address.apartmentNum ?? "",
+    ["Ciudad"]: obj.address.city ?? "",
+    ["Estado"]: obj.address.state ?? "",
+    ["Codigo postal"]: obj.address.zipcode ?? "",
+    ["Telefono"]: obj.phone ?? "",
+    ["Correo"]: obj.email ?? "",
+    ["Tipo de pago"]: obj.paymentMethod ?? "",
+    ["Estatus de pago"]: obj.payed ?? "",
+  }));
+
+  XLSX.utils.sheet_add_json(ws, transformedData, { origin: cell_ref });
+
+  //   for (let i = 0; i < data.length; i++) {
+  //     for (let C = 1; C <= 18; C++) {
+  //       let cell_ref = XLSX.utils.encode_cell(cell_address);
+  //       XLSX.utils.sheet_add_aoa(ws, [[`${R} + ${C}`]], { origin: cell_ref });
+  //     }
+  //   }
+
+  XLSX.utils.book_append_sheet(wb, ws, "Órdenes");
+  XLSX.writeFile(wb, "ordenes.xlsx");
+};
+
+export default OrdersExcelFormatter;
